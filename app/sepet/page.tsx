3c2de80d262dacf5dd095e3f5937c367ebd9b423
dsff012001 <dsff012001@gmail.com>
2026-06-04 22:15:15 +0300

    Add app/sepet/page.tsx

---
+++ app/sepet/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from '@/components/app-shell'
+import { CartPage } from '@/components/cart-page'
+
+export default function Sepet() {
+  return (
+    <AppShell>
+      <CartPage />
+    </AppShell>
+  )
+}
