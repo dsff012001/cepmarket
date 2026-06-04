71f164f55b0a60b030a5999d8094a76468a304e0
dsff012001 <dsff012001@gmail.com>
2026-06-04 22:15:10 +0300

    Add app/favoriler/page.tsx

---
+++ app/favoriler/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from '@/components/app-shell'
+import { FavoritesPage } from '@/components/favorites-page'
+
+export default function Favoriler() {
+  return (
+    <AppShell>
+      <FavoritesPage />
+    </AppShell>
+  )
+}
