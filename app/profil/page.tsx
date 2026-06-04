dde7a467359abd23e8abef82ce715b311ea8e5fd
dsff012001 <dsff012001@gmail.com>
2026-06-04 22:15:14 +0300

    Add app/profil/page.tsx

---
+++ app/profil/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from '@/components/app-shell'
+import { ProfilePage } from '@/components/profile-page'
+
+export default function Profil() {
+  return (
+    <AppShell>
+      <ProfilePage />
+    </AppShell>
+  )
+}
