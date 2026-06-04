b85f2452de9bf36e2c090264f34ebbd822814dc0
dsff012001 <dsff012001@gmail.com>
2026-06-04 22:15:17 +0300

    Add app/siparisler/page.tsx

---
+++ app/siparisler/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from '@/components/app-shell'
+import { OrdersPage } from '@/components/orders-page'
+
+export default function Siparisler() {
+  return (
+    <AppShell>
+      <OrdersPage />
+    </AppShell>
+  )
+}
